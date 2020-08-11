4976fc9c8db5a4ac5eba3ba9aee2c30f7de695f1
Alejandro Santillan <asantillan@modoole.com>
2020-08-11 08:10:37 +0000

    [REF] Unit 4.1: Working on our module

--- openacademy/models/session.py
+++ openacademy/models/session.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from datetime import timedelta
+from odoo import models, fields, api, exceptions
 
 
 class OpenAcademySession(models.Model):
@@ -9,6 +10,7 @@ class OpenAcademySession(models.Model):
 
     name = fields.Char(required=True)
     start_date = fields.Date(default=fields.Date.today)
+    end_date = fields.Date(store=True, compute="_get_end_date", inverse="_set_end_date")
     duration = fields.Float(digits=(6,2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
     instructor_id = fields.Many2one('res.partner', string='instructor')
@@ -21,8 +23,9 @@ class OpenAcademySession(models.Model):
 
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
 
-    taken_seats = fields.Float(compute="_taken_seats");
+    taken_seats = fields.Float(compute="_taken_seats", store=True);
     active = fields.Boolean(default=True)
+
 
     @api.depends('seats', 'attendee_ids')
     def _taken_seats(self):
@@ -31,3 +34,15 @@ class OpenAcademySession(models.Model):
                 record.taken_seats = 0
             else:
                 record.taken_seats = 100.0 * len(record.attendee_ids) / record.seats
+
+
+    @api.depends('start_date', 'duration')
+    def _get_end_date(self):
+        for record in self.filtered('start_date'):
+            record.end_date = record.start_date + timedelta(days=record.duration, seconds=-1)
+
+
+    @api.depends('start_date', 'duration')
+    def _set_end_date(self):
+        for record in self.filtered('start_date'):
+            record.duration = (record.end_date - record.start_date).days + 1
