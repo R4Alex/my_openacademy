7ed27f9e59a0c57b7ef58c26a531e3afd97b5a2a
Alejandro Santillan <asantillan@vauxoo.com>
2020-11-23 22:16:33 +0000

    [DIFF] Check

--- openacademy/models/res_partner.py
+++ openacademy/models/res_partner.py
@@ -3,10 +3,9 @@
 from odoo import models, fields, api
 
 
-class Partner(models.Model):
+class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    instructor = fields.Boolean(default=False)
-    sessions_ids = fields.Many2many('openacademy.session', string="Attended Sessions", readonly=True)
+    instructor = fields.Boolean()
+    sessions_ids = fields.Many2many('openacademy.session', string="Attended Sessions")
     student = fields.Boolean(default=True)
-    other_field = fields.Boolean(default=True)
